22998f031a7a3f091cde192b2a97cb1fe0a644cc
Austin <montaust@umich.edu>
2017-02-13 21:55:43 -0500

    package init

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
 # react-native-stars
-a versatile react native star review component with half star compatibility and custom images, star sizes, star count, star spacing, and value display.
+
+[![NPM](https://nodei.co/npm/react-native-stars.png?downloads=true)](https://nodei.co/npm/react-native-stars/)
+
+`react-native-stars` is a versatile react native star review component with half star compatibility and custom images, star sizes, star count, star spacing, and value display.
+
+<p align="center">
+  <img src="https://github.com/extrct/react-native-stars/master/example-images/rn-stars.gif" alt="basics gif" width="" height=""/>
+  <img src="https://github.com/extrct/react-native-stars/master/example-images/rn-stars-android.gif" alt="basics gif android" width="" height=""/>
+</p>
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+  i. [Basics](#basics)
+  ii. [Modes](#modes)
+  iii. [Props](#props)
+  iv. [Image Input](#image-input)
+3. [Contributing](#contributing)
+
+## Installation
+
+```sh
+npm install react-native-stars --save
+```
+
+## Usage
+
+### Basics
+
+#### For Display
+
+```js
+// ...
+
+<View style={{alignItems:'center'}}>
+  <Stars
+    value={3.67}
+    spacing={8}
+    count={5}
+    starSize={40}
+    backingColor='cornsilk'
+    fullStar= {require('../../images/starFilled.png')}
+    emptyStar= {require('../../images/starEmpty.png')}/>
+</View>
+
+// ...
+```
+
+#### For Input
+
+```js
+// ...
+
+<View style={{alignItems:'center'}}>
+  <Stars
+        half={true}
+        rating={2.5}
+        update={(val)=>{this.setState({stars: val})}}
+        spacing={4}
+        starSize={40}
+        count={5}
+        fullStar={require('./images/starFilled.png')}
+        emptyStar={require('./images/starEmpty.png')}
+        halfStar={require('./images/starHalf.png')}/>
+</View>
+
+// ...
+```
+
+### Modes  
+
+#### Selection Mode  
+
+Rating is dynamic; changes with user input, calls update function prop. This is the default mode. Use the `rating` prop to define a default star value on page load. Use `half` prop to allow for half star input granularity.
+
+#### Display Mode
+
+Rating is static; for displaying an aggregate value as a proportion of stars. Activated by passing a `value` prop - overrides the presence of any Selection Mode specific props. Defaults to bar cutoff proportion - overridden by `opacity` prop which maps star values to opacity as a percentage instead.
+
+### Props
+
+| Prop | Type | Description | Required | Default |
+|---|---|---|---|---|
+|**`fullStar`**|`image`| of the form `require('./path/to/image.png')` |`Yes`|`NA`|
+|**`emptyStar`**|`image`| see above |`Yes`|`NA`|
+|**`halfStar`**|`image`| see above |`No`|`null`|
+|**`count`**|`int`|the total number of stars|`No`|`5`|
+|**`starSize`**|`int`|width,height of individual star|`No`|`30`|
+|**`rating`**|`int or X.5`| (in Selection Mode) initial default rating |`No`|`0`|
+|**`update`**|`function`| (in Selection Mode) function to be run on ratings selection ex: `update={(val)=> this.setState({stars: val})}` |`No`|`()=>{}`|
+|**`spacing`**|`number`| pixel amount of separation between each star|`No`|`0`|
+|**`value`**|`number between 0 and count`| (passing any value sets to Display Mode) star value to be displayed eg. 3.6 (of 5), 7.8 (of 10) |`No`|`null`|
+|**`backingColor`**|`bool`| (in Display Mode) color behind the component (cannot be transparent)|`No`|`white`|
+|**`opacity`**|`bool`|(in Display Mode) displays star values as opacity percentages (0 - 1.0)|`No`|`false`|
+|**`half`**|`bool`|(in Selection Mode) allows for half star ratings|`No`|`false`|
+
+### Image Input
+
+props `fullStar`, `emptyStar`, and `halfStar` expect image files with transparent backgrounds (.pngs), close to uniform dimensions, consistency between images (both fullStar.png and emptyStar.png are 100px/100px). Half star images are expected to be cropped as though they were full, with the center of the image intersecting the right edge of the half star object. If you're using Adobe Illustrator you can check the ['Use Artboards' box](https://github.com/extrct/react-native-stars/master/example-images/export.png) to ensure that empty space in the artboard is included during the export to .png. Half star images can, but don't need to have full outlines - both formats below are acceptible.
+
+example compatible images:
+
+<p align="center">
+  <img src="https://github.com/extrct/react-native-stars/master/example-images/stars.jpg" alt="Example star formats" width="" height=""/>
+</p>
+
+## Contributing
+
+Pull Requests for new features and bugfixes are welcome! :)
+
+## License
+
+[MIT License](http://opensource.org/licenses/mit-license.html)
---
+++ index.js
@@ -0,0 +1,208 @@
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+import {
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+export default class StarReview extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        rating: props.rating
+    };
+
+    this.ratingMode = this.ratingMode.bind(this);
+    this.halfRatingMode = this.halfRatingMode.bind(this);
+    this.displayBar = this.displayBar.bind(this);
+    this.displayOpaque = this.displayOpaque.bind(this);
+    this.star = this.star.bind(this);
+  }
+
+  displayBar(){
+    var partial = this.props.value - Math.floor(this.props.value);
+    var blockStyle = {height: this.props.starSize, width: this.props.starSize * (1.0 - partial), backgroundColor: this.props.backingColor};
+    var emptyBlockStyle = {height: this.props.starSize, width: this.props.starSize * partial, backgroundColor: 'transparent'};
+    var starStyle = {height: this.props.starSize, width: this.props.starSize, backgroundColor:this.props.backingColor};
+    var stars = [];
+      for(var i = 1; i < this.props.count + 1; i++){
+        if(i == Math.floor(this.props.value) + 1){
+          //partial star
+          stars.push(
+            <View key={i} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+              <Image style={starStyle} source={this.props.fullStar}>
+                <View style={{flexDirection: 'row'}}>
+                 <View style={emptyBlockStyle}></View>
+                 <View style={blockStyle}></View>
+                </View>
+                <Image style={{height: this.props.starSize, width: this.props.starSize, backgroundColor:'transparent', position:'absolute'}} source={this.props.emptyStar} />
+              </Image>
+            </View>
+          );
+        }else if(i > Math.floor(this.props.value) + 1){
+          //empty stars
+          stars.push(
+            <View key={i} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+              <Image style={starStyle} source={this.props.emptyStar} />
+            </View>
+          );
+        }else{
+          //filled stars
+          stars.push(
+            <View key={i} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+              <Image style={starStyle} source={this.props.fullStar} />
+            </View>
+          );
+        }
+      }
+      return(
+        <View>
+          <View style={{flexDirection: 'row', justifyContent: 'center'}}>{stars}</View>
+        </View>
+      );
+  }
+
+
+  displayOpaque(){
+    var partial = this.props.value - Math.floor(this.props.value);
+    var starStyle = {height: this.props.starSize, width: this.props.starSize, opacity: 1.0, backgroundColor:'transparent'};
+    var stars = [];
+    for(var i = 1; i < this.props.count + 1; i++){
+      if(i == Math.floor(this.props.value) + 1){
+        //partial star
+        stars.push(
+          <View key={i} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+            <Image style={starStyle} source={this.props.emptyStar}>
+              <Image style={{
+                height: this.props.starSize,
+                width: this.props.starSize,
+                opacity: partial,
+                backgroundColor:'transparent'}} source={this.props.fullStar} />
+            </Image>
+          </View>
+        );
+      }else if(i > Math.floor(this.props.value) + 1){
+        //empty stars
+        stars.push(
+          <View key={i} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+            <Image style={starStyle} source={this.props.emptyStar} />
+          </View>
+        );
+      }else{
+        //filled stars
+        stars.push(
+          <View key={i} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+            <Image style={starStyle} source={this.props.fullStar} />
+          </View>
+        );
+      }
+    }
+    return(
+      <View>
+        <View style={{flexDirection: 'row', justifyContent: 'center'}}>{stars}</View>
+      </View>
+    );
+  }
+
+  halfStar(val,starImg,halfImg){
+    return(
+      <View key={val} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+        <Image style={{width: this.props.starSize, height: this.props.starSize}} source={starImg}>
+          <Image style={{width: this.props.starSize, height: this.props.starSize}} source={halfImg}/>
+        </Image>
+        <View style={{flexDirection: 'row', position: 'absolute'}}>
+              <TouchableOpacity style={{height:this.props.starSize,width:this.props.starSize/2}} onPress={()=>{
+                this.setState({rating: val - 0.5});
+                this.props.update(val - 0.5);
+              }}/>
+              <TouchableOpacity style={{height:this.props.starSize,width:this.props.starSize/2}} onPress={()=>{
+                this.setState({rating: val});
+                this.props.update(val);
+              }}/>
+            </View>
+      </View>
+    );
+  }
+
+  halfRatingMode(){
+    var stars = [];
+    for(var i = 1; i < this.props.count + 1; i++){
+      var starImg = (i <= this.state.rating) ? this.props.fullStar : this.props.emptyStar;
+      var halfImg = (this.state.rating + 0.5 == i) ? this.props.halfStar : null;
+      stars.push(
+        this.halfStar(i,starImg,halfImg)
+      );
+    }
+    return(
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        {stars}
+      </View>
+    );
+  }
+
+  star(val,starImg){
+    return(
+      <View key={val} style={{paddingLeft: this.props.spacing/2, paddingRight: this.props.spacing/2}}>
+        <TouchableOpacity onPress={()=>{
+          this.setState({rating: val});
+          this.props.update(val);
+        }}>
+          <Image style={{width: this.props.starSize, height: this.props.starSize}} source={starImg} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  ratingMode(){
+    var stars = [];
+    for(var i = 1; i < this.props.count + 1; i++){
+      var starImg = (i <= this.state.rating) ? this.props.fullStar : this.props.emptyStar ;
+      stars.push(
+        this.star(i,starImg)
+      );
+    }
+    return(
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        {stars}
+      </View>
+    );
+  }
+
+  render() {
+    let view = this.props.value == null ? (this.props.half ? this.halfRatingMode() :this.ratingMode()) : (this.props.opacity ? this.displayOpaque() : this.displayBar());
+    return (
+      <View>
+        {view}
+      </View>
+    );
+  }
+}
+
+StarReview.propTypes = {
+  value: PropTypes.number,
+  count: PropTypes.number,
+  rating: PropTypes.number,
+  emptyStar: PropTypes.number.isRequired,
+  fullStar: PropTypes.number.isRequired,
+  halfStar: PropTypes.number,
+  update: PropTypes.func,
+  starSize: PropTypes.number,
+  backingColor: PropTypes.string,
+  opacity: PropTypes.bool,
+  half: PropTypes.bool,
+  spacing: PropTypes.number
+};
+
+StarReview.defaultProps = {
+  value: null,
+  count: 5,
+  rating: 0,
+  starSize: 30,
+  update: ()=>{},
+  backingColor: 'white',
+  opacity: false,
+  half: false,
+  spacing: 0
+};
